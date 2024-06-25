cd84db39e5567af9f0bb738d8cf527ccadea96e5
Azizullah Saeidi <azizullahsaeidi@gmail.com>
2024-06-25 18:25:35 +0430

    Remove link

--- resources/js/Pages/Partials/NoRecordFound.vue
+++ resources/js/Pages/Partials/NoRecordFound.vue
@@ -12,7 +12,7 @@
             <Link href="/" class="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
               Go back home
             </Link>
-            <Link href="/time-sheet" class="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+            <Link href="/" class="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
               Check your data
             </Link>
           </div>
@@ -22,7 +22,7 @@
   </div>
 </template>
 <script>
-    
+
 import { Link } from '@inertiajs/inertia-vue3';
 export default({
     components: {
