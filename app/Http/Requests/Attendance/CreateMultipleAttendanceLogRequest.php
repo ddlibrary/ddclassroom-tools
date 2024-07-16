853843d746221703ab21959dbb285ef300973db7
Azizullah Saeidi <azizullahsaeidi@gmail.com>
2024-07-16 16:02:46 +0430

    Create app/Http/Requests/Attendance/CreateMultipleAttendanceLogRequest.php

---
+++ app/Http/Requests/Attendance/CreateMultipleAttendanceLogRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests\Attendance;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class CreateMultipleAttendanceLogRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'month_id' => ['required', 'exists:users,id'],
+            'year' => ['required', 'exists:years,name'],
+            'file' => ['required', 'mimes:xlsx, csv, xls'],
+        ];
+    }
+}
