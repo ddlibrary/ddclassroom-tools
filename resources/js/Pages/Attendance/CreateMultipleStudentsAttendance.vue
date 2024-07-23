41782b30b4148da22e87132002c5b9025ae9b339
Azizullah Saeidi <azizullahsaeidi@gmail.com>
2024-07-23 20:04:21 +0430

    Change middle to midter

--- resources/js/Pages/Attendance/CreateMultipleStudentsAttendance.vue
+++ resources/js/Pages/Attendance/CreateMultipleStudentsAttendance.vue
@@ -75,7 +75,7 @@
                                                 autocomplete="subject"
                                                 class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
                                                 <option value="">...</option>
-                                                <option value="1">Middle Exam</option>
+                                                <option value="1">Midterm Exam</option>
                                                 <option value="2">Final Exam</option>
                                             </select>
                                             <p class="mt-2 text-sm text-red-500" v-if="errors.type">
