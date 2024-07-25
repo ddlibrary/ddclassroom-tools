b6ef02691f74669c64e4820ccd15cc4ad742b3de
Azizullah Saeidi <azizullahsaeidi@gmail.com>
2024-07-25 17:45:32 +0430

    Create app/Http/Requests/Score/MultipleStudentRequest.php Request

---
+++ app/Http/Requests/Score/MultipleStudentRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests\Score;
+
+use App\Enums\SubjectMinScoreEnum;
+use Illuminate\Foundation\Http\FormRequest;
+
+class MultipleStudentRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'sub_grade_id' => ['required', 'exists:sub_grades,id'],
+            'teacher_id' => ['required', 'exists:users,id'],
+            'subject_id' => ['required', 'exists:subjects,id'],
+            'year' => ['required', 'exists:years,name'],
+        ];
+    }
+}
