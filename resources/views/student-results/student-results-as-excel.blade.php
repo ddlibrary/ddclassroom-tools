363b6feffa37b86bb8cd6a16ed01bba724b5f0e0
Azizullah Saeidi <azizullahsaeidi@gmail.com>
2024-08-07 10:55:21 +0430

    update blade file

--- resources/views/student-results/student-results-as-excel.blade.php
+++ resources/views/student-results/student-results-as-excel.blade.php
@@ -29,18 +29,21 @@
                 <th style="text-align: center;border:1px solid #000 !important;text-weight:bold;">NO</th>
                 <th style="text-align: center;border:1px solid #000 !important;text-weight:bold;">Name</th>
                 <th style="text-align: center;border:1px solid #000 !important;text-weight:bold;">Father</th>
+                <th style="text-align: center;border:1px solid #000 !important;text-weight:bold;">نام</th>
+                <th style="text-align: center;border:1px solid #000 !important;text-weight:bold;">نام پدر</th>
+                <th style="text-align: center;border:1px solid #000 !important;text-weight:bold;">Moodle ID</th>
                 <th style="text-align: center;border:1px solid #000 !important;text-weight:bold;">Grade</th>
                 <th style="text-align: center;border:1px solid #000 !important;text-weight:bold;">Year</th>
-                <th style="text-align: left;border:1px solid #000 !important;text-weight:bold;">Middle Score</th>
+                <th style="text-align: left;border:1px solid #000 !important;text-weight:bold;">Midterm Score</th>
                 <th style="text-align: center;border:1px solid #000 !important;text-weight:bold;">Final score</th>
                 <th style="text-align: center;border:1px solid #000 !important;text-weight:bold;">Total Score</th>
-                <th style="text-align: center;border:1px solid #000 !important;text-weight:bold;">Middle Percentage</th>
+                <th style="text-align: center;border:1px solid #000 !important;text-weight:bold;">Midterm Percentage</th>
                 <th style="text-align: center;border:1px solid #000 !important;text-weight:bold;">Final Percentage</th>
                 <th style="text-align: center;border:1px solid #000 !important;text-weight:bold;">Percentage</th>
-                <th style="text-align: center;border:1px solid #000 !important;text-weight:bold;">Middle Result</th>
+                <th style="text-align: center;border:1px solid #000 !important;text-weight:bold;">Midterm Result</th>
                 <th style="text-align: center;border:1px solid #000 !important;text-weight:bold;">Final Result</th>
                 <th style="text-align: center;border:1px solid #000 !important;text-weight:bold;">Result</th>
-                <th style="text-align: center;border:1px solid #000 !important;text-weight:bold;">Middle State</th>
+                <th style="text-align: center;border:1px solid #000 !important;text-weight:bold;">Midterm State</th>
                 <th style="text-align: center;border:1px solid #000 !important;text-weight:bold;">Final State</th>
                 <th style="text-align: center;border:1px solid #000 !important;text-weight:bold;">State</th>
                 <th style="text-align: center;border:1px solid #000 !important;text-weight:bold;">Teacher</th>
@@ -50,8 +53,11 @@
                     <td style="text-align: center;border:1px solid #000 !important">{{ $loop->iteration }}</td>
                     <td style="text-align: center;border:1px solid #000 !important">{{ $result->student->name }}</td>
                     <td style="text-align: center;border:1px solid #000 !important">{{ $result->student->father_name }}
-                    </td>
-                    <td style="text-align: center;border:1px solid #000 !important">{{ $result->subGrade->name }}</td>
+                    <td style="text-align: center;border:1px solid #000 !important">{{ $result->student->fa_name }}
+                    <td style="text-align: center;border:1px solid #000 !important">{{ $result->student->fa_father_name }}
+                    <td style="text-align: center;border:1px solid #000 !important">{{ $result->student->id_number }}</td>
+                    <td style="text-align: center;border:1px solid #000 !important">{{ $result->subGrade->grade_id }} {{ $result
+                    ->subGrade->level }}</td>
                     <td style="text-align: center;border:1px solid #000 !important">{{ $result->year }}</td>
                     <td style="text-align: center;border:1px solid #000 !important">{{ $result->middle }}</td>
                     <td style="text-align: center;border:1px solid #000 !important">{{ $result->final }}</td>
@@ -76,6 +82,27 @@
                 </tr>
             @endforeach
         </table>
+        {{-- <table>
+            <tr>
+                <th>DDC Midterm Exam Result Summary-2024</th>
+                <th></th>
+            </tr>
+            @foreach ($resultCategories as $resultCategory)
+
+            <tr>
+                <td>
+                    Students who scored between {{$resultCategory->from}}% & {{$resultCategory->to}}% ({{$resultCategory->name}})
+                </td>
+                <td>{{ $resultCategory->midterm_student_results_count}}</td>
+
+            </tr>
+            @endforeach
+
+            <tr>
+                <th>Total</th>
+                <th>{{ $resultCategories->sum('midterm_student_results_count') }}</th>
+            </tr>
+        </table> --}}
     </div>
 </body>
 
