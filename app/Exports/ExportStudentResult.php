4d9e086e96e0eb2e96b79a9c45c06a9557f694e5
Azizullah Saeidi <azizullahsaeidi@gmail.com>
2024-08-07 10:53:28 +0430

    Add total result

--- app/Exports/ExportStudentResult.php
+++ app/Exports/ExportStudentResult.php
@@ -2,6 +2,7 @@
 
 namespace App\Exports;
 
+use App\Models\Result;
 use App\Models\StudentResult;
 use Illuminate\Contracts\View\View;
 use Maatwebsite\Excel\Concerns\FromView;
@@ -15,7 +16,7 @@ class ExportStudentResult implements FromView, ShouldAutoSize
     {
         $year = $this->data['year'];
         $gradeId = $this->data['grade_id'];
-        $results = StudentResult::with(['student:id,name,father_name,uuid', 'middleResult:id,name', 'finalResult:id,name', 'result:id,name', 'teacher:id,name'])
+        $results = StudentResult::with(['student:id,name,father_name,uuid,id_number,fa_name,fa_father_name,email,password', 'middleResult:id,name', 'finalResult:id,name', 'result:id,name', 'teacher:id,name'])
             ->where('year', $year)
             ->where(function ($query) use ($gradeId) {
                 if ($gradeId) {
@@ -33,8 +34,18 @@ class ExportStudentResult implements FromView, ShouldAutoSize
             ])
             ->get();
 
+        $resultCategories = Result::withCount(['midtermStudentResults' => function($query) use ($year, $gradeId){
+            $query->where('year', $year);
+            if ($gradeId) {
+                $query->where('sub_grade_id', $gradeId);
+            }
+        }],'middle_result_id')->get();
+
+        info($resultCategories);
+
         return view('student-results.student-results-as-excel', [
             'results' => $results,
+            'resultCategories' => $resultCategories
         ]);
     }
 }
