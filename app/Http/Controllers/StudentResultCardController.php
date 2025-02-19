3cc5d299654d5dfa3fef3714a8ea95a7a4be18a0
Azizullah Saeidi <azizullahsaeidi@gmail.com>
2025-02-19 13:44:14 +0430

    Add sub_grade_id on condition

--- app/Http/Controllers/StudentResultCardController.php
+++ app/Http/Controllers/StudentResultCardController.php
@@ -6,6 +6,7 @@ use App\Models\ClassResponsible;
 use App\Models\GradeSubject;
 use App\Models\Result;
 use App\Models\Student;
+use App\Models\StudentResult;
 use Illuminate\Http\Request;
 
 class StudentResultCardController extends Controller
@@ -17,22 +18,20 @@ class StudentResultCardController extends Controller
         return view('students.show', compact('student'));
     }
 
-    public function studentResultCard(Request $request, $uuid, $year)
+    public function studentResultCard(Request $request, $uuid, $year, StudentResult $studentResult)
     {
+        $subGradeId = $studentResult->sub_grade_id;
         $student = Student::where('uuid', $uuid)
-            ->with('studentResult', function ($query) use ($year) {
+            ->with('middleAttendance', function ($query) use ($year, $subGradeId) {
                 $query->where([
                     'year' => $year,
+                    'sub_grade_id' => $subGradeId
                 ]);
             })
-            ->with('middleAttendance', function ($query) use ($year) {
+            ->with('finalAttendance', function ($query) use ($year, $subGradeId) {
                 $query->where([
                     'year' => $year,
-                ]);
-            })
-            ->with('finalAttendance', function ($query) use ($year) {
-                $query->where([
-                    'year' => $year,
+                    'sub_grade_id' => $subGradeId
                 ]);
             })
             ->firstOrFail();
@@ -46,6 +45,7 @@ class StudentResultCardController extends Controller
         $where = [
             'year' => $year,
             'student_id' => $studentId,
+            'sub_grade_id' => $subGradeId
         ];
 
         $subjects = GradeSubject::with(['grade:id,name', 'subject'])
@@ -62,10 +62,10 @@ class StudentResultCardController extends Controller
         $results = Result::all();
 
         if ($request->en_result) {
-            return view('students.student-en-result-card', compact('student', 'subjects', 'results', 'responsible'));
+            return view('students.student-en-result-card', compact('student', 'subjects', 'results', 'responsible', 'studentResult'));
         }
 
-        return view('students.student-result-card', compact('student', 'subjects', 'results', 'responsible', 'year'));
+        return view('students.student-result-card', compact('student', 'subjects', 'results', 'responsible', 'year', 'studentResult'));
     }
 
     public function resultCards() {}
