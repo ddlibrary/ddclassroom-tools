6e790684698e8ebfaafd4da72ad79574bd698d42
Azizullah Saeidi <azizullahsaeidi@gmail.com>
2025-05-31 11:14:29 +0430

    Fix attendance total records

--- resources/views/attendance/export-attendance-log-report-as-excel.blade.php
+++ resources/views/attendance/export-attendance-log-report-as-excel.blade.php
@@ -50,7 +50,7 @@
                 <td  style="text-align: center;border:1px solid #000 !important">{{ $student->excused_count }}</td>
                 <td  style="text-align: center;border:1px solid #000 !important">
                 <?php
-                    $absent = (isset($student->attendance_logs_count) && ($student->attendance_logs_count > 0)) ? ($student->absent_count * 100 / $student->attendance_logs_count) : 0;
+                    $absent = $$student->attendance_logs_count == 0 ? 100 : (isset($student->attendance_logs_count) && ($student->attendance_logs_count > 0)) ? ($student->absent_count * 100 / $student->attendance_logs_count) : 0;
                 ?>
                 {{ $absent >= 20 ? 'No' : 'Yes'}}
                 </td>
