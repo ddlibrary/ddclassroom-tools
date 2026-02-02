fedebc1017c797d960158fa2057231aded7db497
Azizullah <azizullahsaeidi@gmail.com>
2026-02-02 14:21:24 +0430

    use the filter and remove the duplicated filterring

--- app/Exports/ExportRetakeOpportunities.php
+++ app/Exports/ExportRetakeOpportunities.php
@@ -4,7 +4,6 @@ namespace App\Exports;
 
 use App\Models\StudentRetakeOpportunity;
 use Illuminate\Contracts\View\View;
-use Illuminate\Http\Request;
 use Maatwebsite\Excel\Concerns\FromView;
 use Maatwebsite\Excel\Concerns\ShouldAutoSize;
 
@@ -14,40 +13,16 @@ class ExportRetakeOpportunities implements FromView, ShouldAutoSize
 
     public function view(): View
     {
-        $request = new Request($this->filters);
-
-        $query = StudentRetakeOpportunity::query()
+        $retakeOpportunities = StudentRetakeOpportunity::query()
             ->with([
                 'student:id,name,fa_name,father_name,fa_father_name,email,id_number',
                 'subGrade:id,name,full_name',
                 'year:id,name',
                 'subject:id,name'
-            ]);
-
-        if ($request->search) {
-            $search = $request->search;
-            $query->whereHas('student', function ($q) use ($search) {
-                $q->whereAny(['name', 'fa_name', 'email', 'id_number'], 'like', "%$search%");
-            });
-        }
-
-        if ($request->sub_grade_id) {
-            $query->where('sub_grade_id', $request->sub_grade_id);
-        }
-
-        if ($request->year_id) {
-            $query->where('year_id', $request->year_id);
-        }
-
-        if ($request->subject_id) {
-            $query->where('subject_id', $request->subject_id);
-        }
-
-        if ($request->has('is_passed') && $request->is_passed !== '') {
-            $query->where('is_passed', $request->is_passed == '1' ? true : false);
-        }
-
-        $retakeOpportunities = $query->orderByDesc('id')->get();
+            ])
+            ->filter($this->filters)
+            ->orderByDesc('id')
+            ->get();
 
         return view('exports.retake-opportunities-report', [
             'retakeOpportunities' => $retakeOpportunities,
--- app/Http/Controllers/StudentRetakeOpportunityController.php
+++ app/Http/Controllers/StudentRetakeOpportunityController.php
@@ -14,38 +14,16 @@ class StudentRetakeOpportunityController extends Controller
 {
     public function index(Request $request)
     {
-        $query = StudentRetakeOpportunity::query()
+        $retakeOpportunities = StudentRetakeOpportunity::query()
             ->with([
                 'student:id,name,fa_name,father_name,fa_father_name,email,id_number',
                 'subGrade:id,name,full_name',
                 'year:id,name',
                 'subject:id,name'
-            ]);
-
-        if ($request->search) {
-            $search = $request->search;
-            $query->whereHas('student', function ($q) use ($search) {
-                $q->whereAny(['name', 'fa_name', 'email', 'id_number'], 'like', "%$search%");
-            });
-        }
-
-        if ($request->sub_grade_id) {
-            $query->where('sub_grade_id', $request->sub_grade_id);
-        }
-
-        if ($request->year_id) {
-            $query->where('year_id', $request->year_id);
-        }
-
-        if ($request->subject_id) {
-            $query->where('subject_id', $request->subject_id);
-        }
-
-        if ($request->has('is_passed') && $request->is_passed !== '') {
-            $query->where('is_passed', $request->is_passed == '1' ? true : false);
-        }
-
-        $retakeOpportunities = $query->orderByDesc('id')->paginate(50);
+            ])
+            ->filter($request)
+            ->orderByDesc('id')
+            ->paginate(50);
         $grades = SubGrade::whereIsActive(true)->get();
         $years = Year::all(['id', 'name']);
         $subjects = Subject::all(['id', 'name']);
