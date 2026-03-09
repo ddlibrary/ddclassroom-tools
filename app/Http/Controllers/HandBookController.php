48de892e69deaaa4475a285e336319af899dd2b5
Azizullah <azizullahsaeidi@gmail.com>
2026-03-09 13:45:51 +0430

    Apply student filter for handbooks

--- app/Http/Controllers/HandBookController.php
+++ app/Http/Controllers/HandBookController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Jobs\SendEmailJob;
 use App\Models\Student;
 use App\Models\SubGrade;
+use App\Models\Year;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
 
 class HandBookController extends Controller
@@ -13,44 +15,21 @@ class HandBookController extends Controller
     {
         $query = Student::query()->with(['country:id,name', 'subGrade']);
 
-        if ($request->username) {
-            $search = $request->username;
-            $query->where(function ($query) use ($search) {
-                $query->where('username', $search)
-                    ->orWhere('email', $search);
-            });
-        }
-
-        if ($request->grade_id) {
-            $query->where('sub_grade_id', $request->grade_id);
-        }
+        $query = $this->applyStudentFilters($query, $request);
 
         $students = $query->orderBy('id', 'desc')->paginate(100);
 
+        $years = Year::all();
         $grades = SubGrade::whereIsActive(true)->get();
 
-        return inertia('Handbook/Index', ['students' => $students, 'grades' => $grades]);
+        return inertia('Handbook/Index', ['students' => $students, 'grades' => $grades, 'years' => $years]);
     }
 
     public function sendHandbook(Request $request)
     {
         $query = Student::query();
 
-        if ($request->username) {
-            $search = $request->username;
-            $query->where(function ($query) use ($search) {
-                $query->where('username', $search)
-                    ->orWhere('email', $search);
-            });
-        }
-
-        if ($request->country_id) {
-            $query->where('country_id', $request->country_id);
-        }
-
-        if ($request->grade_id) {
-            $query->where('sub_grade_id', $request->grade_id);
-        }
+        $query = $this->applyStudentFilters($query, $request);
 
         $students = $query->get();
 
@@ -61,5 +40,44 @@ class HandBookController extends Controller
         }
 
         return back();
+    }
+
+    private function applyStudentFilters(Builder $query, Request $request): Builder
+    {
+        $yearId  = $request->input('year_id');
+        $gradeId = $request->input('grade_id');
+
+        if ($yearId) {
+            $year = Year::find($yearId);
+            if ($year) {
+                $query->whereHas('enrollments', function ($q) use ($year, $gradeId) {
+                    $q->where([
+                        'year' => $year->name,
+                        'is_active' => true,
+                    ]);
+                    if ($gradeId) {
+                        $q->where('sub_grade_id', $gradeId);
+                    }
+                });
+            }
+        }
+
+        if ($request->filled('username')) {
+            $search = $request->username;
+            $query->where(function ($q) use ($search) {
+                $q->where('username', $search)
+                ->orWhere('email', $search);
+            });
+        }
+
+        if ($request->filled('country_id')) {
+            $query->where('country_id', $request->country_id);
+        }
+
+        if ($gradeId) {
+            $query->where('sub_grade_id', $gradeId);
+        }
+
+        return $query;
     }
 }
